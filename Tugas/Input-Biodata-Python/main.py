f9ded90c570bbbd1b6f9b8834f5422a7171f4d3f
Ignasius Silaban <92926515+Tior3turn@users.noreply.github.com>
2021-10-28 23:55:39 +0700

    Create main.py

---
+++ Tugas/Input-Biodata-Python/main.py
@@ -0,0 +1,22 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+nama = input("Nama Lengkap  : ")
+inisial = input("Nama Panggilan  : ")
+nim = input("NPM  : ")
+tl = input("Tempat Lahir  : ")
+ttl = input("Umur  : ")
+telp = input("Nomor Telepon  : ")
+almt = input("Alamat  : ")
+print("Assalamu'alaikum")
+print("Letme introduce my self. My name is"+nama)
+print(", but you can call me"+inisial)
+print(". My NPM is"+nim)
+print(".I was born in"+tl)
+print( "and i am"+ttl)
+print(" years old.I am very glad if you want to invite my house in"+almt)
+print(". So, don't forget to call me before with the number"+telp)
+print("Thank you.")
