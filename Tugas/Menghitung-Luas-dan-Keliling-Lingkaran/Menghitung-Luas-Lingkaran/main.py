6c80ede44baa1771bdb967d122502e09e6451706
Ignasius Silaban <92926515+Tior3turn@users.noreply.github.com>
2021-11-04 21:49:29 +0700

    Update main.py

--- Tugas/Menghitung-Luas-dan-Keliling-Lingkaran/Menghitung-Luas-Lingkaran/main.py
+++ Tugas/Menghitung-Luas-dan-Keliling-Lingkaran/Menghitung-Luas-Lingkaran/main.py
@@ -1,1 +1,15 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+print('Menghitung luas lingakaran')
+print('==============================')
+Jar = int(input('masukan jari-jari lingkaran: '))
+phi = 3.14
+Luas = phi * (Jar * Jar)
+print('Luas lingakaran dengan jari-jari {} adalah {}'.format(Jar, Luas))
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
 
