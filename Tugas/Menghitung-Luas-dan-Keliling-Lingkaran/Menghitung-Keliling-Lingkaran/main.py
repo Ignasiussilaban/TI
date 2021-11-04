5e493fd4b8de1c37da0903f5bdfac6d5a78c2f1c
Ignasius Silaban <92926515+Tior3turn@users.noreply.github.com>
2021-11-04 21:44:56 +0700

    Update main.py

--- Tugas/Menghitung-Luas-dan-Keliling-Lingkaran/Menghitung-Keliling-Lingkaran/main.py
+++ Tugas/Menghitung-Luas-dan-Keliling-Lingkaran/Menghitung-Keliling-Lingkaran/main.py
@@ -1,1 +1,13 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+print (" Program Perhitungan Keliling Lingkaran")
+jar = float(input(" Masukkan Jari-Jari : "))
+hasil = 2 * jar * 3.14
+print(" Keliling Lingkaran adalah : "+ str(hasil))
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
 
